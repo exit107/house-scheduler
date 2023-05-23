74ccbafa65e74c43edf76c3f25496a3fb6784d7c
Dave Brown <dave.brown@exit107.com>
2023-05-23 14:20:07 -0600

    Migrated to pydantic Fixes Migrate to pydantic #2

--- house_scheduler/models.py
+++ house_scheduler/models.py
@@ -1,22 +1,13 @@
 from datetime import datetime
 
+from pydantic import BaseModel
 
-class Task:
-    def __init__(
-        self,
-        name,
-        description: str,
-        start,
-        end: datetime,
-        repeat: str = "",
-        all_day: bool = False,
-    ) -> None:
-        self.name = name
-        self.description = description
-        self.start = start
-        self.end = end
-        self.repeat = repeat
-        self.all_day = all_day
 
-    def __repr__(self) -> str:
-        return f"<Task({self.name})>"
+class Task(BaseModel):
+    id: int
+    name: str
+    description: str = ""
+    start: datetime
+    end: datetime | None = None
+    repeat: str = ""
+    all_day: bool = False
